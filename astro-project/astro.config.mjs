12a6323b379c76768d9d2aae32e3cce156da20fc
Julian Porath <Julian.Porath@wolters-rundreisen.de>
2024-12-27 07:24:45 +0100

    - task with solution - they will be deleted in the future

--- astro-project/astro.config.mjs
+++ astro-project/astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    experimental: {
+        svg: true,
+    },
+    adapter: node({
+        mode: "standalone",
+    }),
+    output: "server",
+    integrations: [vue(), svelte()],
+});
---
+++ astro-project/src/components/newSvelte.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+<h1>Moin</h1>
+<style>
+
+</style>
